a0c76dab34ae04af7de9bd6659b525c71ae206af
weli <l.weinan@gmail.com>
2023-08-18 00:04:44 +0800

    add func

--- src/Foo.vue
+++ src/Foo.vue
@@ -1,9 +1,22 @@
 <template>
   <h2 class="heading">Foo</h2>
+  <h2 class="heading">I am {{reverseMessage(message)}}</h2>
+  <input v-model="message" type="text" placeholder="Enter your message"/>
 </template>
 <script lang="ts">
+
 export default {
   name: 'Foo',
+  data() {
+    return {
+      message: '',
+    };
+  },
+  methods: {
+    reverseMessage(message: string): string {
+      return message.split('').reverse().join('')
+    },
+  },
 };
 </script>
 <style>
