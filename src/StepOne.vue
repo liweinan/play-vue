b3ab4cb7a062d473a57eb8b63eeb6ffd3aac7f85
weli <l.weinan@gmail.com>
2023-08-29 11:37:07 +0800

    const comp

--- src/StepOne.vue
+++ src/StepOne.vue
@@ -3,12 +3,28 @@
     <label for="name">Step one's input</label>
     <input v-model="name" id="name"/>
   </div>
+  <p>
+    <StepTwo/>
+    <!--    <component :is="StepTwo"/>-->
+  </p>
 </template>
 
-
 <script lang="ts">
-import { defineComponent } from 'vue'
+import {defineComponent} from 'vue'
 
+export const StepTwo = {
+  template: `
+    <div>
+      <label for="name">Step two's input</label>
+      <input v-model="name" id="name"/>
+    </div>
+  `,
+  data() {
+    return {
+      name: "",
+    }
+  },
+}
 export default defineComponent({
   name: 'StepOne',
   data() {
@@ -16,5 +32,11 @@ export default defineComponent({
       name: 'Hello StepOne!'
     }
   },
+  // computed: {
+  //   StepTwo() {
+  //     return StepTwo
+  //   }
+  // },
+  components: {StepTwo}
 })
 </script>
