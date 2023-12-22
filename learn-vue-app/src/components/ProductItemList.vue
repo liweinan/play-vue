f7251fa0e96af314b0e14950966c6ffe10215a72
XiaoLi <leonpa1987@gmail.com>
2023-12-22 21:37:47 +0800

    add learn vue test code

---
+++ learn-vue-app/src/components/ProductItemList.vue
@@ -0,0 +1,25 @@
+<script setup>
+import ListLayout from './ListLayout.vue'
+</script>
+
+<!-- ProductItemList.vue -->
+<template>
+  <div id="app">
+    <ListLayout>
+      <template #thumbnail="{ item }">
+        <img
+            v-if="item.thumbnail"
+            class="list-layout__item__thumbnail"
+            :src="item.thumbnail"
+            :alt="item.name"
+            width="200"
+        />
+      </template>
+      <template #actions>
+        <div class="list-layout__item__footer">
+          <button class="list-layout__item__footer__button">Add to cart</button>
+        </div>
+      </template>
+    </ListLayout>
+  </div>
+</template>
---
+++ learn-vue-app/src/main.js
@@ -0,0 +1,10 @@
+// import './assets/main.css'
+
+import { createApp, ref } from 'vue'
+// import { createApp, ref } from 'vue/dist/vue.esm-bundler';
+import App from './App.vue'
+// import App from './components/MyApp.vue'
+
+const app = createApp(App)
+
+app.mount('#app')
