1e44ecbe845d178b75de9880769dd47189e2360a
XiaoLi <leonpa1987@gmail.com>
2024-01-01 16:50:18 +0800

    update

--- learn-vue-app/src/router/index.ts
+++ learn-vue-app/src/router/index.ts
@@ -13,6 +13,8 @@ import ContactView from "../views/ContactView.vue";
 import ContactFormView from "../views/ContactFormView.vue";
 import ContactFaqView from "../views/ContactFaqView.vue";
 import LoginView from "../views/LoginView.vue";
+import PizzaView from "../views/PizzaView.vue";
+import ErrorView from "../views/ErrorView.vue";
 
 const routes:RouteRecordRaw[]  = [
     {
@@ -26,12 +28,21 @@ const routes:RouteRecordRaw[]  = [
         component: AboutView
     },
     {
-        path: '/pizzas',
-        name: 'pizzas',
-        component: PizzasView,
+        path: "/pizzas",
         props: (route: RouteLocationNormalizedLoaded) => ({
             searchTerm: route.query?.search || "",
         }),
+
+        children: [{
+            path: ':id',
+            name: 'pizza',
+            component: PizzaView,
+            props: true,
+        }, {
+            path: '',
+            name: 'pizzas',
+            component: PizzasView,
+        }]
     },
     {
         path: "/contact",
@@ -59,6 +70,11 @@ const routes:RouteRecordRaw[]  = [
         path: '/login',
         name: 'login',
         component: LoginView
+    },
+    {
+        path: '/:pathMatch(.*)*',
+        name: 'error',
+        component: ErrorView
     }
 ]
 
